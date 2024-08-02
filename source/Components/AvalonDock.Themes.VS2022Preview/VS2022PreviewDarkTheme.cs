52c99ccd9be8e57664c1f72d08ced13c68711b6e
MLing592 <58897757+MLing592@users.noreply.github.com>
2024-08-02 23:12:48 +0800

    feat(*): Copy VS2022 To VS2022Preview

---
+++ source/Components/AvalonDock.Themes.VS2022Preview/VS2022PreviewDarkTheme.cs
@@ -0,0 +1,25 @@
+/************************************************************************
+   AvalonDock
+
+   Copyright (C) 2007-2013 Xceed Software Inc.
+
+   This program is provided to you under the terms of the Microsoft Public
+   License (Ms-PL) as published at https://opensource.org/licenses/MS-PL
+ ************************************************************************/
+
+using System;
+using System.Windows.Media;
+
+namespace AvalonDock.Themes;
+/// <inheritdoc/>
+public class VS2022PreviewDarkTheme : Theme
+{
+	/// <inheritdoc/>
+	public override Uri GetResourceUri()
+	{
+		return new Uri(
+			"/AvalonDock.Themes.VS2022Preview;component/DarkTheme.xaml",
+			UriKind.Relative);
+	}
+
+}
